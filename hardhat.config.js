8865820dffe9728ae8e2c6ec75be4cb29cab4a4d
smartman1234 <john.smith.dev0724@gmail.com>
2022-07-14 22:59:48 +0100

    feat : deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,11 +10,17 @@ require('dotenv').config();
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: "polygon_mumbai",
   networks: {
-    hardhat: {
-    }
+    hardhat: {},
+    polygon_mumbai: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+   }
   },
   solidity: {
     version :"0.8.7",
@@ -25,6 +31,7 @@ module.exports = {
       },
     },
   },
+  
   paths: {
     sources: "./contracts",
     tests: "./test",
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -24,6 +24,13 @@ const main = async () => {
     const address = JSON.stringify({
       "contractAddress": PropertyNftContract.address,
     })
+    const token_address = JSON.stringify({
+        "contractAddress": tokenContract.address,
+    })
+
+    const vault_address = JSON.stringify({
+        "contractAddress": SavingVaultContract.address,
+    })
   
     console.log("Property nft address: ", PropertyNftContract.address);
     console.log("Token address:", tokenContract.address);
@@ -33,11 +40,17 @@ const main = async () => {
 
   
     const abi = fs.readFileSync(`./artifacts/contracts/PropertyNFT.sol/Property.json`);
+    const token_abi = fs.readFileSync(`./artifacts/contracts/TestToken/test.sol/TestToken.json`);
+    const vault_abi = fs.readFileSync(`./artifacts/contracts/Vault.sol/SavingVault.json`);
   
     fs.writeFileSync('./src/contracts/abi.json', abi);
     fs.writeFileSync('./src/contracts/contract_address.json', address)
+
+    fs.writeFileSync('./src/contracts/token_abi.json', token_abi);
+    fs.writeFileSync('./src/contracts/token_address.json', token_address)
   
-    
+    fs.writeFileSync('./src/contracts/vault_abi.json', vault_abi);
+    fs.writeFileSync('./src/contracts/vault_address.json', vault_address)
 }
     
 const runMain = async () => {
